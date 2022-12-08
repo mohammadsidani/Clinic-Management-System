b9710b2656ee0e34721f93d19436f67705169d65
Mohammad Sidani <mohammadsidani13@gmail.com>
2022-12-08 20:41:56 +0200

    Update Patient.java

--- src/Patients/Patient.java
+++ src/Patients/Patient.java
@@ -1,1 +1,24 @@
+package Patients;
 
+import Utility.Person;
+
+public class Patient extends Person {
+
+	public Patient() {
+	}
+
+	public Patient(int id, String name, String address, int phoneNumber, int age, String gender) {
+		super(id, name, address, phoneNumber, age, gender);
+	}
+
+	public String toString() {
+		System.out.println("The patient's Id:" + getId()+"\n");
+		System.out.println("The patient's name:" + getName()+"\n");
+		System.out.println("The patient's gender:" + getGender()+"\n");
+		System.out.println("The patient's age:" + getAge()+"\n");
+		System.out.println("The patient's phone number:" + getPhoneNumber()+"\n");
+		System.out.println("The patient's address:" + getAddress()+"\n");
+		System.out.println("*******************************NEW PATIENT*********************************\n");
+		return null;
+	}
+}
